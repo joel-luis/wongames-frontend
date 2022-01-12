a86eecfd40fb1c2a874179debf61c4219a0cefc3
Joel <joelluis73@gmail.com>
2022-01-12 14:58:06 -0300

    create Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,38 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead is back!',
+    subtitle: 'Come see John`s new adventures',
+    backgroundImage: '/images/red-dead-img.jpeg',
+    buttonLabel: 'Buy now',
+    buttonLink: 'https'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+Default.parameters = {
+  layout: 'fullscreen'
+}
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/images/red-dead-float.png'
+}
+
+WithFloatImage.parameters = {
+  layout: 'fullscreen'
+}
