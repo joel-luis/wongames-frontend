bfaea093c8419756f3ac22471eb11ebb20820951
Joel <joelluis73@gmail.com>
2022-01-20 15:52:11 -0300

    create GameDetails Component

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,67 @@
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
+
+import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+type Platform = 'windows' | 'linux' | 'mac'
+
+export type GameDetailsProps = {
+  platforms: Platform[]
+}
+
+const GameDetails = ({ platforms }: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="Linux" size={18} />,
+    mac: <Apple title="Mac" size={18} />,
+    windows: <Windows title="windows" size={18} />
+  }
+
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
+
+      <S.Content>
+        <S.Block>
+          <S.Title>Developer</S.Title>
+          <S.Description>Gearbox Software</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Release Date</S.Title>
+          <S.Description>Nov 16, 2019 </S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Plataform</S.Title>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Publisher</S.Title>
+          <S.Description>2K</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Rating</S.Title>
+          <S.Description>18+</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Title>Genres</S.Title>
+          <S.Description>Action / Adventure</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default GameDetails
---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameDetails from '.'
+
+describe('<GameDetails />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(<GameDetails />)
+  })
+})
